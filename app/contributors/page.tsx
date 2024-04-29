fc54e75c91685e65a63ed517df86cf8a6a00eb7e
Richard Shaju <richardshaju66@gmail.com>
2024-04-30 00:52:57 +0530

    Small UI fixe

--- app/contributors/page.tsx
+++ app/contributors/page.tsx
@@ -9,13 +9,12 @@ import { AvatarCard } from "@/components/ui/avatar-card";
 import CardGrid from "@/components/ui/CardGrid";
 import { FaGithub } from "react-icons/fa";
 
-
 function Page() {
   const [data, setData] = useState<any>([]);
   useEffect(() => {
-   fetch(GITHUB_API_URL)
+    fetch(GITHUB_API_URL)
       .then(async (resp) => {
-        setData(await resp.json())
+        setData(await resp.json());
       })
       .catch((error) => {
         console.error("An error occurred:", error);
@@ -25,26 +24,40 @@ function Page() {
   return data.length == 0 ? (
     <Loading msg="Loading..." />
   ) : (
-    <div className="flex flex-col dark:bg-[#121212] min-h-[50rem] h-[100dvh] ">
+    <div className="flex flex-col dark:bg-[#121212] ">
       <Navbar />
-      <div className="flex justify-center mb-8 flex-col">
-        <div className="text-2xl flex items-center justify-center mb-5">Contributors</div>
-      <CardGrid>
-          {data.map((contr:{login:string, avatar_url:string, html_url:string, }) => (
-            <div key={contr.login} className="rounded-[24px] flex flex-col border border-slate-600 p-5">
+      <div className="flex justify-center mb-8 flex-col h-[35rem] ">
+        <div className="text-2xl flex items-center justify-center mb-5">
+          Contributors
+        </div>
+        <CardGrid>
+          {data.map(
+            (contr: {
+              login: string;
+              avatar_url: string;
+              html_url: string;
+            }) => (
+              <div
+                key={contr.login}
+                className="rounded-[24px] flex flex-col border border-slate-600 p-5"
+              >
                 <AvatarCard url={contr.avatar_url} />
-                <a href={contr.html_url} target="_blank" className="transition-all hover:scale-105 scale-100">
-                  <div className="flex justify-center gap-2 items-center mt-5 rounded-lg bg-slate-800 p-2">
-                      <FaGithub size={30}/>
-                      <span className="text-md">/ @{contr.login}</span>
+                <a
+                  href={contr.html_url}
+                  target="_blank"
+                  className="transition-all hover:scale-105 scale-100"
+                >
+                  <div className="flex justify-center gap-2 items-center mt-5 rounded-lg bg-white dark:bg-slate-800 p-2">
+                    <FaGithub size={30} />
+                    <span className="text-md">@{contr.login}</span>
                   </div>
                 </a>
-            </div>
-          ))}
+              </div>
+            )
+          )}
         </CardGrid>
-      
       </div>
-        <Footer/>
+        <Footer />
     </div>
   );
 }
