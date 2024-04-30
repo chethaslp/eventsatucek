fb2fd20c3b266c32584736510f4e4e5ca954a342
chethas.lp <lchethas@gmail.com>
2024-04-30 15:43:10 +0530

    +fix metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,40 @@ import { Raleway } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/components/context/theme'
 import { Toaster } from '@/components/ui/toaster'
+import { Metadata, ResolvingMetadata } from 'next'
+import { getEvent, getImgLink } from '@/lib/data'
 
 const inter = Raleway({ subsets: ['latin'] })
-
+ 
+type Props = {
+  params: { id: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+ 
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const evnt = (await getEvent(params.id))[0]
+ 
+  return {
+    title: evnt[4],
+    description: evnt[5],
+    openGraph: {
+      title: evnt[4],
+      images: getImgLink(evnt[6]),
+      description: evnt[5],
+      type:"article",
+      authors:evnt[7]
+    },
+    twitter:{
+      title: evnt[4],
+      images: getImgLink(evnt[6]),
+      description: evnt[5],
+      card:"summary_large_image"
+    }
+  }
+}
 
 export default function RootLayout({
   children,
