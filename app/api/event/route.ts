75eedabed23ff11e5010c51c96dd02eeacb014af
Richard Shaju <richardshaju66@gmail.com>
2024-05-22 02:42:22 +0530

    Renvenza logo added

--- app/api/event/route.ts
+++ app/api/event/route.ts
@@ -107,6 +107,7 @@ function resolveClubIcon(clb: string): any {
     "Music Club - UCEK": "/logos/mcc.png",
     "Film Club - UCEK": "/logos/fmc.png",
     "NSS - UCEK": "/logos/nss.png",
+    "Renvnza '24": "/logos/renvnza.png",
   }[clb];
 }
 function resolveClubEmail(clb: string): any {
